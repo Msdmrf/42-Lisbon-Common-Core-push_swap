c2fb2780e22c15933b667e9ca07cf079fb65c395
Miguel Santos De Matos Ramirez Fernandes <migusant@c2r2s8.42lisboa.com>
2025-05-30 12:40:11 +0100

    Add empty file check to main.c

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: migusant <migusant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 15:55:21 by migusant          #+#    #+#             */
-/*   Updated: 2025/05/29 12:09:27 by migusant         ###   ########.fr       */
+/*   Updated: 2025/05/30 12:39:40 by migusant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	t_stack	*a;
 	t_stack	*b;
 
-	if (argc < 2)
+	if (argc < 2 || argv[1][0] == '\0')
 		return (0);
 	a = init_stack_a(argc, argv);
 	if (is_sorted(a))
