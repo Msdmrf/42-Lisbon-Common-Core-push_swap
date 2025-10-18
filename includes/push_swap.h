0d9faae3af70c8115a801d14a26bf1682f3c6fea
Miguel Ramirez <miguel.matos.fernandes1@gmail.com>
2025-10-18 19:57:58 +0100

    Final header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: migusant <migusant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 15:54:37 by migusant          #+#    #+#             */
-/*   Updated: 2025/06/09 15:06:57 by migusant         ###   ########.fr       */
+/*   Updated: 2025/10/15 22:13:22 by migusant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,52 +30,53 @@ typedef struct s_stack
 	char	id;
 }	t_stack;
 
-// Error Handling
+// Stack Management (stack_management.c)
+t_stack		*create_stack(char id);
+void		push(t_stack *stack, int value);
+void		free_stack(t_stack *stack);
+
+// Stack Utilities (stack_utils.c)
+int			find_min(t_stack *stack);
+int			find_position(t_stack *stack, int value);
+int			is_sorted(t_stack *stack);
+
+// Parsing (parsing.c)
+t_stack		*parse_arguments(int argc, char **argv);
+
+// Parsing (parsing_utils.c)
+int			process_numbers(t_stack *stack, char *str);
+
+// Error Handling (error_handling.c)
 void		error_exit(void);
 void		cleanup_and_exit(t_stack *a, t_stack *b, int exit_code);
 
-// Parsing
-t_stack		*parse_arguments(int argc, char **argv);
-int			has_content(const char *str);
-void		free_split(char **split);
-int			is_valid_number(char *str);
-int			ft_atoi_safe(const char *str, int *number);
-int			process_numbers(t_stack *stack, char *str);
-
-// Stack Initialization
+// Stack Initialization (stack_init.c)
 t_stack		*init_stack_a(int argc, char **argv);
 t_stack		*init_stack_b(t_stack *a);
 
-// Stack Management
-t_stack		*create_stack(char id);
-void		push(t_stack *stack, int value);
-int			pop(t_stack *stack);
-void		free_stack(t_stack *stack);
-
-// Stack Operations (with silent flag for bonus part)
+// Stack Swap Operations (stack_ops_swap.c)
 void		sa(t_stack *a, int silent);
 void		sb(t_stack *b, int silent);
 void		ss(t_stack *a, t_stack *b, int silent);
+
+// Stack Push Operations (stack_ops_push.c)
 void		pa(t_stack *a, t_stack *b, int silent);
 void		pb(t_stack *a, t_stack *b, int silent);
+
+// Stack Rotate Operations (stack_ops_rotate.c)
 void		ra(t_stack *a, int silent);
 void		rb(t_stack *b, int silent);
 void		rr(t_stack *a, t_stack *b, int silent);
+
+// Stack Reverse Rotate Operations (stack_ops_reverse_rotate.c)
 void		rra(t_stack *a, int silent);
 void		rrb(t_stack *b, int silent);
 void		rrr(t_stack *a, t_stack *b, int silent);
 
-// Stack Utilities
-int			find_min(t_stack *stack);
-int			find_max(t_stack *stack);
-int			find_position(t_stack *stack, int value);
-int			is_sorted(t_stack *stack);
-
-// Sorting Algorithms
-void		sort_two(t_stack *a);
-void		sort_three(t_stack *a);
-void		sort_n(t_stack *a, t_stack *b, int n);
-void		sort_radix(t_stack *a, t_stack *b);
+// Small Stacks Algorithm (sort_small.c)
 void		sort_dispatch(t_stack *a, t_stack *b);
+
+// Large Stacks Algorithm (sort_big.c)
+void		sort_radix(t_stack *a, t_stack *b);
 
 #endif
